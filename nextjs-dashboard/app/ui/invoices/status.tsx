2522862669989425c6f0952c0aabf2ddea697e99
Ho Nyeong <dev.honing@gmail.com>
2023-12-21 17:09:47 +0900

    status.tsx 상세 주석을 작성하고, InvoiceStatus 컴포넌트 import

--- nextjs-dashboard/app/ui/invoices/status.tsx
+++ nextjs-dashboard/app/ui/invoices/status.tsx
@@ -3,23 +3,30 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
+// InvoiceStatus 컴포넌트 정의
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
+    // span 태그에 Tailwind CSS 클래스 조합 적용
     <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        'inline-flex items-center rounded-full px-2 py-1 text-xs', {
+          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-green-500 text-white': status === 'paid',
+        },
       )}
     >
       {status === 'pending' ? (
+      // 'pending' 상태일 때의 UI
         <>
-          Pending
-          <ClockIcon className="ml-1 w-4 text-gray-500" />
+          Pending {/* 텍스트 표시 */}
+          <ClockIcon className="ml-1 w-4 text-gray-500" /> {/* 시계 아이콘 표시 */}
         </>
       ) : null}
       {status === 'paid' ? (
+        // 'paid' 상태일 때의 UI
         <>
-          Paid
-          <CheckIcon className="ml-1 w-4 text-white" />
+          Paid {/* 텍스트 표시 */}
+          <CheckIcon className="ml-1 w-4 text-white" /> {/* 체크 아이콘 표시 */}
         </>
       ) : null}
     </span>
