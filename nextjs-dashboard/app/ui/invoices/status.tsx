c9f47338864fd653b955daf98aa70ac0d7abe45b
Ho Nyeong <dev.honing@gmail.com>
2023-12-21 16:27:15 +0900

    파일 경로를 주석으로 작성

--- nextjs-dashboard/app/ui/invoices/status.tsx
+++ nextjs-dashboard/app/ui/invoices/status.tsx
@@ -1,3 +1,5 @@
+// app/ui/invoices/status.tsx
+
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 
@@ -6,10 +8,6 @@ export default function InvoiceStatus({ status }: { status: string }) {
     <span
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
-        {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
-        },
       )}
     >
       {status === 'pending' ? (
