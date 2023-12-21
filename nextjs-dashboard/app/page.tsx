bda113c6e03cb4f60659b7b0217590ae04bbc539
Ho Nyeong <dev.honing@gmail.com>
2023-12-21 18:35:09 +0900

    Lusitana 폰트 적용

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,10 +1,12 @@
 // app/page.tsx
 
 import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon, ClockIcon } from '@heroicons/react/24/outline'; // 시계 아이콘 추가
+import { ArrowRightIcon, ClockIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import InvoiceStatus from '@/app/ui/invoices/status'; // InvoiceStatus 추가
+import InvoiceStatus from '@/app/ui/invoices/status';
+import { lusitana } from '@/app/ui/font'; // lusitana 폰트 추가
+
 
 export default function Page() {
   return (
@@ -15,7 +17,8 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape}></div>
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+    >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
